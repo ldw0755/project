7fd30ad8307082058abb37d632fc0159f7cf8f21
sooseong <tjtntjd82@naver.com>
2020-11-03 17:25:25 +0900

    Hell.java - getter, setter 추가

--- src/project/Hello.java
+++ src/project/Hello.java
@@ -13,4 +13,29 @@ public class Hello {
 	public void displayInfor() {
 		System.out.println("이름:"+name+" 나이:"+age+" 지역:"+place);
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getPlace() {
+		return place;
+	}
+
+	public void setPlace(String place) {
+		this.place = place;
+	}
+	
 }
