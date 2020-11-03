fb298d7de1dbde5d97d6e2e75aa61d478cc26966
thanatus76 <thanatus76@gmail.com>
2020-11-03 17:28:23 +0900

    Collatz

---
+++ src/project/Collatz.java
@@ -0,0 +1,28 @@
+package project;
+
+import java.util.Scanner;
+
+public class Collatz {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner stdIn = new Scanner(System.in);
+		long num = stdIn.nextInt();
+		
+		while (true) {
+			System.out.print(num + " ");
+			
+			if (num == 1) {
+		            break;
+			}
+
+			if (num % 2 == 0) {
+				num /= 2;
+			} else {
+				num = num * 3 + 1;
+			}
+		}
+		
+		System.out.println();
+	}
+}
